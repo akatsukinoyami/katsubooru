d6a46aca19ed3b24d644a0aa7c792196d48c3b06
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:31 +0200

    Added footer to App

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-	import { Router } from "svelte-navigator";
+  import { Router } from 'svelte-navigator';
 
-  import Aside from "./components/Aside.svelte";
-  import Main from "./components/Main.svelte";
-  import Navbar from "./components/Navbar.svelte";
+  import Navbar from './partials/Navbar.svelte';
+  import Main from './partials/Main.svelte';
+  import Footer from './partials/Footer.svelte';
 </script>
 
-<Router primary={false}>
-    <Navbar/>
-    <Aside />
-    <Main />
+<Router>
+  <Navbar/>
+  <Main/>
+  <Footer/>
 </Router>
 
 <style lang="sass">
   :global(html),
   :global(body)
     padding: 0
+    display: flex
+    min-height: 100vh
+    flex-direction: column
+
+  :global(main)
+    flex: 1 0 auto
 </style>
--- client/src/partials/Aside.svelte
+++ client/src/partials/Aside.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import Col from '../components/Col.svelte';
-
-  import { asideActive  } from '../stores';
 </script>
 
 <Col size={3}>
