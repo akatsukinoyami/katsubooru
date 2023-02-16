e754e5201bfb4807c0d27c7e7642158c6f80a829
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:31 +0200

    changes in components

--- client/src/components/Col.svelte
+++ client/src/components/Col.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import Preloader from "./Preloader.svelte";
 
-  type columnSizeType = 0|1|2|3|4|5|6|7|8|9|10|11|12;
+  type columnSizeType = -1|0|1|2|3|4|5|6|7|8|9|10|11|12;
 
   export let
-    className = '',
-    size: columnSizeType = 0;
+    className: string = '',
+    s: columnSizeType = -1,
+    m: columnSizeType = -1,
+    l: columnSizeType = -1,
+    xl:columnSizeType = -1;
 
-  let columnSize = size ? `s${size}` : '';
+  function sizeClass(columnType: string, columnSize: number): string {
+    return columnSize >= 0 ? `${columnType}${columnSize} ` : ''
+  }
 </script>
 
-<div class="col {className} {columnSize}">
+<div class="col {sizeClass('s', s)}{sizeClass('m', m)}{sizeClass('l', l)}{sizeClass('xl', xl)} {className}">
 	<slot><Preloader/></slot>
 </div>
---
+++ client/src/components/Main.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Preloader from "./Preloader.svelte";
+
+  export let className = '';
+</script>
+
+<main class="container {className}">
+	<slot><Preloader/></slot>
+</main>
+
+<style lang="sass">
+  main
+    flex: 1 0 auto
+</style>
