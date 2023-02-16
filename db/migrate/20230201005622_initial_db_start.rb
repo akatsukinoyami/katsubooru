0f8011d3ad48f03c096a4328534dd3cf71fd1d9c
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:29 +0200

    create initial migration

---
+++ db/migrate/20230201005622_initial_db_start.rb
@@ -0,0 +1,30 @@
+class InitialDbStart < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :avatar
+
+      t.timestamps
+    end
+
+    create_table :entities do |t|
+      t.string :type, null: false, default: 'picture'
+      t.string :file, null: false
+
+      t.string :origin
+      t.string :origin_type
+
+      t.references :user
+      t.timestamps
+    end
+
+    create_table :tags do |t|
+      t.string :type
+      t.string :name
+
+      t.timestamps
+    end
+
+    create_join_table :entities, :tags
+  end
+end
