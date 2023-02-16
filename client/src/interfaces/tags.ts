1e599343c91e3c8529b5faf0cf98214bca68c330
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-02-16 18:55:32 +0200

    added tags interface

---
+++ client/src/interfaces/tags.ts
@@ -0,0 +1,8 @@
+import type TagInterface from "./tag"
+
+export default interface TagsInterface {
+  artist?: TagInterface[],
+  tags?: TagInterface[],
+  titles?: TagInterface[],
+  characters?: TagInterface[],
+}
