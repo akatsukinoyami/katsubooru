22a4c72eb2ac2827c280cb7bb7b0eef3287a6f20
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:31 +0200

    added body padding 0

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { Router } from "svelte-navigator";
-  import "smelte/src/tailwind.css" ;
 
   import Aside from "./components/Aside.svelte";
   import Main from "./components/Main.svelte";
   import Navbar from "./components/Navbar.svelte";
 </script>
-
 
 <Router primary={false}>
     <Navbar/>
@@ -15,4 +13,7 @@
 </Router>
 
 <style lang="sass">
+  :global(html),
+  :global(body)
+    padding: 0
 </style>
