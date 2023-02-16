1a715229e2e99338cd76c6623dc1af8ceac7beb0
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:31 +0200

    add container component

---
+++ client/src/components/Container.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Preloader from "./Preloader.svelte";
+
+  export let className = '';
+</script>
+
+<div class="container {className}">
+	<slot><Preloader/></slot>
+</div>
