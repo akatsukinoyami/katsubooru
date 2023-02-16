7956dc4a14d7f48275376de6864e03a0fdc22fd7
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:30 +0200

    remade auth method

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -6,13 +6,26 @@ class Api::ApiController < ApplicationController
 
   private
 
-  def authenticate_request
+  def authenticate
     header = request.headers["Authorization"]
     token = header.split.last if header
     decoded = jwt_decode(token)
-  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-    render json: { error: "Unauthorized" }, status: :unauthorized
-  else
     @current_user = User.find(decoded[:user_id])
+
+  rescue JWT::DecodeError
+    return 'errors.token_not_found'
+  rescue ActiveRecord::RecordNotFound
+    return 'errors.token_invalid'
+  else
+    return nil
+  end
+
+  def authenticate_request
+    error = authenticate
+
+    render(
+      json: { errors: [I18n.t(error)] },
+      status: :unauthorized
+    ) if error
   end
 end
