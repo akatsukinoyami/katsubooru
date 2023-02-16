5080aebee08b09c321302e744cecf87da2ccd941
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:29 +0200

    add origins for entities

--- db/migrate/20230201005622_create_entities.rb
+++ db/migrate/20230201005622_create_entities.rb
@@ -1,8 +1,11 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.string :type
-      t.string :file
+      t.string :type, null: false, default: 'picture'
+      t.string :file, null: false
+
+      t.string :origin
+      t.string :origin_type
 
       t.timestamps
     end
