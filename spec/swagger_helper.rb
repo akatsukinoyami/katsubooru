0b06b6d7cea1ac9c02f22264cf260197674bea75
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:29 +0200

    replaced swagger.yml to public

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -6,7 +6,7 @@ RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
-  config.swagger_root = Rails.root.join('swagger').to_s
+  config.swagger_root = Rails.root.join('public').to_s
 
   # Define one or more Swagger documents and provide global metadata for each one
   # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
 
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
+    'swagger.yaml' => {
       openapi: '3.0.1',
       info: {
         title: 'API V1',
