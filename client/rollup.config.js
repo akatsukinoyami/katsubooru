4b7f77468fed033ab408375d1f22b6a53f3de671
AkatsukiNoYami <akatsukinoyami@yahoo.com>
2023-02-16 17:33:31 +0200

    refactored rollup config, added copy and sass

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,12 +1,13 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
-import sveltePreprocess from 'svelte-preprocess';
-import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
-import svelte from 'rollup-plugin-svelte';
-import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only';
+import commonjs         from "@rollup/plugin-commonjs";
+import resolve          from "@rollup/plugin-node-resolve";
+import typescript       from "@rollup/plugin-typescript";
+import copy             from "rollup-plugin-copy";
+import css              from "rollup-plugin-css-only";
+import livereload       from "rollup-plugin-livereload";
+import replaceHtmlVars  from "rollup-plugin-replace-html-vars";
+import svelte           from "rollup-plugin-svelte";
+import { terser }       from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 const fingerprint = new Date().valueOf()
@@ -21,42 +22,49 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
+			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+				stdio: ["ignore", "inherit", "inherit"],
 				shell: true
 			});
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
+			process.on("SIGTERM", toExit);
+			process.on("exit", toExit);
 		}
 	};
 }
 
 export default {
-	input: 'src/main.ts',
+	input: "src/main.ts",
 	output: {
 		sourcemap: true,
-		format: 'iife',
-		name: 'app',
+		format: "iife",
+		name: "app",
     entryFileNames: `bundle.${fingerprint}.js`,
-    dir: '../public/build', // Path for storing prebuilt bundle files
+    dir: "../public/build", // Path for storing prebuilt bundle files
 	},
 	plugins: [
     // If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
+		// npm, you"ll most likely need these plugins. In
+		// some cases you"ll need additional configuration -
 		// consult the documentation for details:
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
     commonjs(),
+		resolve({ browser: true, dedupe: ["svelte"] }),
+		typescript({ sourceMap: true, inlineSources: !production }),
 
-		resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
+    // we"ll extract any component CSS out into a separate file - better for performance
+    css({ output: `bundle.${fingerprint}.css`, sourceMap: true }),
+
+    // Replace fingerprints of prebuilt bundle in index.html
+    replaceHtmlVars({ files: "../public/index.html", from: /[0-9]{13,13}/gi, to: fingerprint }),
 
 		svelte({
 			preprocess: sveltePreprocess({
-        sourceMap: !production
+        sourceMap: !production,
+        sass: {
+          sync: true,
+          implementation: require('sass'),
+        },
       }),
       compilerOptions: {
         dev: !production,             // enable run-time checks when not in production
@@ -64,35 +72,18 @@ export default {
       }
 		}),
 
-		typescript({
-      sourceMap: true, //!production,
-			inlineSources: !production
-		}),
 
-    // we'll extract any component CSS out into a separate file - better for performance
-    css({
-      output: `bundle.${fingerprint}.css`,
-      sourceMap: true
-    }),
-
-    // Replace fingerprints of prebuilt bundle in index.html
-    replaceHtmlVars({
-      files: '../public/index.html',
-      from: [
-        /bundle.[0-9]+.js/i,
-        /bundle.[0-9]+.css/i
+    // Copy files from node_modules to build
+    copy({
+      targets: [
+        { src: "node_modules/svelte-material-ui/bare.css", dest: "../public/build/", rename: `material_ui.${fingerprint}.css` }
       ],
-      to: [
-        `bundle.${fingerprint}.js`,
-        `bundle.${fingerprint}.css`
-      ],
+      verbose: !production
     }),
 
 		!production && serve(),               // In dev mode, call `npm run start` once the bundle has been generated
-		!production && livereload('public'),  // Watch the `public` directory and refresh the browser on changes when not in production
-		production && terser()                // If we're building for production (npm run build instead of npm run dev), minify
+		!production && livereload("public"),  // Watch the `public` directory and refresh the browser on changes when not in production
+		production && terser()                // If we"re building for production (npm run build instead of npm run dev), minify
 	],
-	watch: {
-		clearScreen: false
-	}
+	watch: { clearScreen: false }
 };
