f56e5a915a5ee9fef349ab75a02b6b18a9456c62
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-02-17 18:58:09 +0200

    add method to user

--- client/src/stores/currentUser.ts
+++ client/src/stores/currentUser.ts
@@ -1,8 +1,17 @@
-import { writable } from "svelte/store";
+import { get, Writable, writable } from "svelte/store";
 
 const storedCurrentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-export const currentUser = writable(storedCurrentUser || {});
+interface currentUserInterface extends Writable<any> {
+  exists: () => boolean;
+}
+
+export const currentUser: currentUserInterface = {
+  ...writable(storedCurrentUser || {}),
+  exists: function(){
+    return Object.keys(get(this)).length > 0;
+  }
+}
 
 currentUser.subscribe((value) => {
   localStorage.setItem("currentUser", JSON.stringify(value))
