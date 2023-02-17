ca156b09f0fb1588ff6bb92779929a7fffe005f1
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-02-17 16:17:13 +0200

    deleted layout component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-  import Layout from './partials/Layout.svelte';
+  import { Router } from 'svelte-navigator';
   import Routes from './partials/Routes.svelte';
+  import Main from './components/Main.svelte';
+
+  import Alerts from "./partials/_Alerts.svelte";
+  import Navbar from './partials/_Navbar.svelte';
+  import Footer from './partials/_Footer.svelte';
 
   import { title } from "./stores";
 </script>
@@ -9,15 +14,20 @@
   <title>{$title}</title>
 </svelte:head>
 
-<Layout>
-  <Routes/>
-</Layout>
+<Router primary={false}>
+  <Navbar/>
+  <Main>
+    <Alerts/>
+    <Routes/>
+  </Main>
+  <Footer/>
+</Router>
 
 <style lang="sass">
   :global(html),
   :global(body)
     padding: 0
-    display: flex
     min-height: 100vh
+    display: flex
     flex-direction: column
 </style>
