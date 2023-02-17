f7ab679999b29837a96c04b643d9db9ae545eb71
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-02-17 16:16:55 +0200

    add route for show action

--- client/src/partials/Routes.svelte
+++ client/src/partials/Routes.svelte
@@ -2,9 +2,17 @@
   import { Route } from "svelte-navigator";
   import SignIn from "../routes/auth/SignIn.svelte";
   import SignUp from "../routes/auth/SignUp.svelte";
-  import Index from '../routes/entity/Index.svelte';
+
+  import EntitiesIndex from '../routes/entities/index/Page.svelte';
+  import EntitiesShow from '../routes/entities/show/Page.svelte';
 </script>
 
-<Route path="/sign_in" component="{SignIn}" />
-<Route path="/sign_up" component="{SignUp}" />
-<Route component="{Index}" />
+<Route path="auth/*">
+  <Route path="sign_in" component="{SignIn}" />
+  <Route path="sign_up" component="{SignUp}" />
+</Route>
+<Route path="entities/*">
+  <Route path=":id" component="{EntitiesShow}" />
+  <Route path="/" component="{EntitiesIndex}" />
+</Route>
+<Route component="{EntitiesIndex}" />
