757a9fc24335cfda69d2c6ddbee357cc060520a9
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-02-17 16:17:25 +0200

    Make preloader centered

--- client/src/components/Preloader.svelte
+++ client/src/components/Preloader.svelte
@@ -1,3 +1,10 @@
-<div class="progress">
-  <div class="indeterminate"></div>
+<div class="valign-wrapper">
+  <div class="progress">
+    <div class="indeterminate"></div>
+  </div>
 </div>
+
+<style lang="sass">
+  .valign-wrapper
+    height: 100%
+</style>
