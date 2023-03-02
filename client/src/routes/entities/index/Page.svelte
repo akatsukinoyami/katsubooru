8be5477ef7f124d9069834d16cb7c2d32f033ce1
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-03-02 23:49:48 +0200

    FE: refactoring in i18n

--- client/src/routes/entities/index/Page.svelte
+++ client/src/routes/entities/index/Page.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
   import Row from "../../../components/Row.svelte";
   import Preloader from "../../../components/Preloader.svelte";
-  import { entities } from "../../../stores";
   import Main from './_Main.svelte';
   import Aside from './_Aside.svelte';
+  import { entities } from "../../../stores";
+  import i18n from "../../../i18n";
 </script>
 
-{#await entities.all()}
+{#await entities.load()}
   <Preloader/>
 {:then}
   {#if !entities.empty()}
     <Row>
-      <Main  entities={entities.array()}/>
-      <Aside entities={entities.array()}/>
+      <Main/>
+      <Aside/>
     </Row>
+  {:else}
+    {i18n().errors.empty.entities}
   {/if}
+{:catch}
+  {i18n().errors.load.entities}
 {/await}
