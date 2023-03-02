7475950f9fe0ec3e57b1337d3b403ed0e6083093
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-03-02 23:43:45 +0200

    FE: add types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,2 +1,6 @@
+import type { EntityInterface } from "./interfaces";
+
 export type columnSizeType = -1|0|1|2|3|4|5|6|7|8|9|10|11|12;
-export type TagType = Record<number, string>;
+export type TagType = Record<string, string>;
+export type TagsType = Record<string, number[]>;
+export type EntitiesObjectType = Record<number, EntityInterface>;
