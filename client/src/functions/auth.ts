47adcb8286d4bc7c3d9e159d01e10432b3d2e29e
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-03-02 23:44:19 +0200

    FE: fix alerts on auth

--- client/src/functions/auth.ts
+++ client/src/functions/auth.ts
@@ -1,7 +1,6 @@
-import { currentUser } from "../stores/currentUser";
+import { alerts, currentUser } from "../stores";
 import { POST } from "../functions/request";
 import { getValueById } from "../functions/helper";
-import alertsHandler from "../functions/alerts";
 import i18n from "../i18n/i18n";
 
 const t = i18n()
@@ -16,7 +15,7 @@ export async function signIn(): Promise<void> {
     callbackFn: (data: Record<any, any>) => {
       if (data.name){
         currentUser.set(data);
-        alertsHandler.showNotice(t.succesful.sign_in)
+        alerts.showNotice(t.succesful.sign_in)
       }
     },
   })
@@ -33,7 +32,7 @@ export async function signUp(): Promise<void> {
     callbackFn: (data: Record<any, any>) => {
       if (data.name) {
         currentUser.set(data);
-        alertsHandler.showNotice(t.succesful.sign_up)
+        alerts.showNotice(t.succesful.sign_up)
       }
     },
   })
@@ -41,5 +40,5 @@ export async function signUp(): Promise<void> {
 
 export function signOut(): void {
   currentUser.set({})
-  alertsHandler.showNotice(t.succesful.sign_out)
+  alerts.showNotice(t.succesful.sign_out)
 }
