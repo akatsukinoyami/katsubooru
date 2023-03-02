6fb8827dc4362c0a1921d7d3ba68cedd5633aca0
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-03-02 23:39:22 +0200

    FE: made tags.load on routes other then auth

--- client/src/Routes.svelte
+++ client/src/Routes.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { useNavigate, Route } from "svelte-navigator";
+  import { Route } from "svelte-navigator";
 
   import SignIn from "./routes/auth/SignIn.svelte";
   import SignUp from "./routes/auth/SignUp.svelte";
@@ -7,15 +7,26 @@
   import EntitiesIndex from './routes/entities/index/Page.svelte';
   import EntitiesShow from './routes/entities/show/Page.svelte';
   import EntitiesForm from './routes/entities/form/Page.svelte';
+  import Preloader from "./components/Preloader.svelte";
+
+  import { tags } from "./stores";
+  import i18n from "./i18n/i18n";
 </script>
 
 <Route path="auth/*">
   <Route path="sign_in" component="{SignIn}" />
   <Route path="sign_up" component="{SignUp}" />
 </Route>
-<Route path="entities/*">
-  <Route path="/" component="{EntitiesIndex}" />
-  <Route path=":id" component="{EntitiesShow}" />
-  <Route path="/create" component="{EntitiesForm}" />
-</Route>
-<Route component="{EntitiesIndex}" />
+
+{#await tags.load()}
+  <Preloader/>
+{:then}
+  <Route path="entities/*">
+    <Route path="/" component="{EntitiesIndex}" />
+    <Route path=":id" component="{EntitiesShow}" />
+    <Route path="/create" component="{EntitiesForm}" />
+  </Route>
+  <Route component="{EntitiesIndex}" />
+{:catch}
+  {i18n().errors.load.tags}
+{/await}
