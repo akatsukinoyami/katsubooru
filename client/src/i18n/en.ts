cf06a79ec43e9f739548d5020cbd438b3ae04ee0
akatsukinoyami <akatsukinoyami@yahoo.com>
2023-03-02 23:44:54 +0200

    made i18n as ts-file

---
+++ client/src/i18n/en.ts
@@ -0,0 +1,17 @@
+export default {
+  "succesful": {
+    "sign_in": "You signed in succesfully!",
+    "sign_up": "You signed up succesfully!",
+    "sign_out": "You signed out succesfully!"
+  },
+  "error": {
+    "load": {
+      "entities": "Entities failed to load! Please try again later or report to developer.",
+      "tags": "Tags failed to load! Please try again later or report to developer.",
+    },
+    "empty": {
+      "entities": "Entities list is empty. Maybe this is some error. Please report to developer.",
+      "tags": "Tags list is empty. Maybe this is some error. Please report to developer.",
+    }
+  }
+}
